0ac055fd8ec56a0210c2d0381fde5bf93522a455
Nastya2103 <99600134+Nastya2103@users.noreply.github.com>
2024-03-04 13:22:59 +0300

    Update Employee.java

--- src/main/java/org/example/Employee.java
+++ src/main/java/org/example/Employee.java
@@ -5,8 +5,4 @@ public class Employee extends Person {
     public Employee(String name, String post, String email, long phone, int salary, int age) {
         super(name, post, email, phone, salary, age);
     }
-
-    public void printInfo() {
-        System.out.println(String.format("Сотрудник - %s (должность - %s, возраст - %s лет). %nДанные сотрудника: телефон - %s, e-mail - %s, зарплата - %s руб.%n", name, post, age, phone, email, salary));
-    }
 }
